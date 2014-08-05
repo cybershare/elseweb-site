6e4d4cd3735aa809ba7a2579a90786b7e8f8806a
Luis Garnica <lagc03@gmail.com>
2014-08-05 10:19:58 -0600

    Separated website from elseweb project. Initial commit

---
+++ application/views/pages/home.php
@@ -0,0 +1,32 @@
+    <!--container start-->
+    <div class="container">
+        <div class="row">
+            <!--feature start-->
+            <div class="text-center feature-head">
+                <h2>Welcome to the Earth, Life, and Semantic Web Project</h2>
+            </div>
+            
+            <p>The Earth, Life and Semantic Web (ELSeWeb) project integrates the NASA-funded <a href="http://edac.unm.edu">Earth Data Analysis Center</a> with an analytical Web Service platform, <a href="http://lifemapper.org">Lifemapper</a>, which models potential future species distributions under scenarios of climate change. The integrated system provides climate change impact scientists streamlined mechanisms for discovering, accessing, understanding, and using Earth observation data to model future species projection scenarios.</p>
+
+            <!--feature end-->
+        </div>
+
+     <!--property start-->
+    <div class="property gray-bg">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 col-sm-6 text-center">
+                  
+                    <!-- <img width="500" src="<?php echo base_url(IMAGES."banner/chihuahuan-desert-spread.png");?>" alt="text img"> -->
+                </div>
+                <div class="col-lg-6 col-sm-6">
+                    <h4>Where have all the grasslands gone?</h4>
+                    <p>In a span of just over a hundred years, large portions of grasslands encompassing what is now the Chihuahuan desert have been replaced with more rugged desert type flora including the creosote bush (Larrea tridentata). ELSEWeb aims to provide scientists a means to understand what past factors lead to proliferation of desert flora in the southwest and what future factors may cause the further proliferation of these species. By understanding these factors, scientists may make strongly-backed recommendations regarding land and water use.
+                    <br>
+                    <a href="<?php echo base_url("experiments");?>" class="btn btn-purchase">Start Generating Predictions</a></p>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+    <!--property end-->
