76bff82480c5cfecaa4c5df81e2fa023f4707dd5
Luis Garnica <lagc03@gmail.com>
2015-02-18 14:39:31 -0700

    + Production Version 2/18/2015

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -15,15 +15,17 @@
     <div class="property gray-bg">
         <div class="container">
             <div class="row">
-                <div class="col-lg-6 col-sm-6 text-center">
+                <!-- <div class="col-lg-6 col-sm-6 text-center"> -->
                   
                     <!-- <img width="500" src="<?php echo base_url(IMAGES."banner/chihuahuan-desert-spread.png");?>" alt="text img"> -->
-                </div>
-                <div class="col-lg-6 col-sm-6">
-                    <h4>Where have all the grasslands gone?</h4>
+                <!-- </div> -->
+                <div class="col-lg-12 col-sm-12">
+                    <h4 class="text-center">Where have all the grasslands gone?</h4>
                     <p>In a span of just over a hundred years, large portions of grasslands encompassing what is now the Chihuahuan desert have been replaced with more rugged desert type flora including the creosote bush (Larrea tridentata). ELSEWeb aims to provide scientists a means to understand what past factors lead to proliferation of desert flora in the southwest and what future factors may cause the further proliferation of these species. By understanding these factors, scientists may make strongly-backed recommendations regarding land and water use.
                     <br>
-                    <a href="<?php echo base_url("experiments");?>" class="btn btn-purchase">Start Generating Predictions</a></p>
+                    <div class="col-md-12 text-center"> 
+                        <a href="<?php echo base_url("experiments");?>" class="btn btn-purchase">Start Generating Predictions</a></p>
+                    </div>
                 </div>
             </div>
         </div>
