ad9d3c58cba313c8fc96ffdc81d7bbf2e6681c45
Luis Garnica <lagc03@gmail.com>
2015-05-01 11:02:22 -0600

    Added new ontology diagrams

--- application/views/pages/ontologies.old.php
+++ application/views/pages/ontologies.old.php
@@ -10,19 +10,18 @@
 
         <div class="row">
             <div class="hiring">
-                <div class="col-lg-12 col-sm-12">
+                <div class="col-lg-6 col-sm-6">
                     <div class="content">
-                        <h3 class="title">ELSEWeb Data and ELSEWeb EDAC</h3>
-                        <p> The elseweb-data ontology (blue nodes) provides concepts for describing characteristics of
-                            a dataset in a biodiversity scenario: spatial/temporal dimensions, how it can be accessed and
-                            entities described. The elseweb-edac ontology (yellow nodes), extends and instantiates the
-                            elseweb-data ontology to describe data sets published by EDAC. Dashed boxes outline concepts of
-                            the ontology that extend existing ontologies and vocabularies.
-                        </p>
-                        <a href="<?php echo ONTPREFIX."elseweb-edac.owl";?>"><img class="img-responsive displayed" width="850" src="<?php echo base_url(IMAGES."elseweb-data-v2.png");?>" alt="elseweb-data"></a>
-                        <a href="http://ontology.cybershare.utep.edu/ELSEWeb/elseweb-data.owl#">http://ontology.cybershare.utep.edu/ELSEWeb/elseweb-data.owl#</a> <br>
-                        <a href="http://ontology.cybershare.utep.edu/ELSEWeb/elseweb-edac.owl#">http://ontology.cybershare.utep.edu/ELSEWeb/elseweb-edac.owl#</a><br>
-                        <a href="http://ontology.cybershare.utep.edu/ELSEWeb/mappings/elseweb-mappings.owl#">http://ontology.cybershare.utep.edu/ELSEWeb/mappings/elseweb-mappings.owl#</a>
+                        <h3 class="title">ELSEWeb Data</h3>
+                        <p>The <a href="<?php echo ONTPREFIX."elseweb-data.owl";?>">elseweb-data ontology</a> provides concepts for describing the spatial/temporal dimensions of a dataset, how it can be accessed, and the specific nature of the data being contained.</p>
+                        <a href="<?php echo ONTPREFIX."elseweb-data.owl";?>"><img class="img-responsive" width="470" src="<?php echo base_url(IMAGES."elseweb-data.png");?>" alt="elseweb-data"></a>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-sm-6">
+                    <div class="content">
+                        <h3 class="title">ELSEWeb Model</h3>
+                        <p>The <a href="<?php echo ONTPREFIX."elseweb-modelling.owl";?>">elseweb-modelling</a> ontology provides concepts for describing generic algorithms and how they relate to parameter sets and software agents which serve as implementations.</p>
+                         <a href="<?php echo ONTPREFIX."elseweb-modelling.owl";?>"><img class="img-responsive" width="470" src="<?php echo base_url(IMAGES."elseweb-modelling.png");?>" alt="elseweb-modelling"></a>
                     </div>
                 </div>
             </div>
@@ -30,17 +29,18 @@
 
         <div class="row">
             <div class="hiring">
-                <div class="col-lg-12 col-sm-12">
+                <div class="col-lg-6 col-sm-6">
                     <div class="content">
-                        <h3 class="title">ELSEWeb Modeling and ELSEWeb Lifemapper</h3>
-                        <p>The elseweb-model ontology (blue nodes) provides concepts for describing biodiversity modeling algorithms, their inputs, outputs, parameters, and services providing these algorithms. The elseweb-lifemapper ontology (yellow nodes), extends and instantiates the elseweb-model ontology to describe SDM services provided by Lifemapper. Dashed boxes outline concepts of the ontology that extend existing ontologies and vocabularies.</p>
+                        <h3 class="title">ELSEWeb EDAC</h3>
+                        <p>The <a href="<?php echo ONTPREFIX."elseweb-edac.owl";?>">elseweb-edac ontology</a> describes specific raster and vector datasets served by <a href="http://edac.unm.edu/">EDAC’s</a> large collection of Open Geospatial Consortium (OGC) web services. The ontology provides properties for attaching pointers to Web Coverage Service (WCS) Get-Capabilities documents as well as EDAC’s special condensed JSON capabilities representation. The <a href="<?php echo LODSPEAKR."instances/elseweb_edac:PublishedPRISMDataset";?>">environmental datasets</a> contained in this ontology were harvested from the <a href="http://gstore.unm.edu/">EDAC’s GSTORE</a>..</p>
+                         <a href="<?php echo ONTPREFIX."elseweb-edac.owl";?>"><img class="img-responsive" width="470" src="<?php echo base_url(IMAGES."elseweb-edac.png");?>" alt="elseweb-edac"></a>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-sm-6">
+                    <div class="content">
+                        <h3 class="title">ELSEWeb Lifemapper</h3>
                         <p>The <a href="<?php echo ONTPREFIX."elseweb-lifemapper.owl";?>">elseweb-lifemapper</a> describes specific <a href="<?php echo LODSPEAKR."instances/elseweb_lifemapper:LifemapperAlgorithm";?>">modeling algorithms</a> supported by <a href="http://lifemapper.org">Lifemapper</a> as well as corresponding parameter sets. The parameter descriptions contain minimum and maximum “hasValue” property restrictions to define the legal ranges of values bound to specific parameters. This ontology also contains a collection of <a href="<?php echo LODSPEAKR."instances/elseweb_lifemapper:SpeciesOccurrenceDataset";?>">species occurrence data</a> that can be referenced in experiments.</p>
-                        <a href="<?php echo ONTPREFIX."elseweb-lifemapper.owl";?>"><img class="img-responsive displayed" width="850" src="<?php echo base_url(IMAGES."elseweb-modeling-v2.jpeg");?>" alt="elseweb-modeling"></a>
-                        <a href="http://ontology.cybershare.utep.edu/ELSEWeb/elseweb-modelling.owl#">http://ontology.cybershare.utep.edu/ELSEWeb/elseweb-modelling.owl#</a><br>
-                        <a href="http://ontology.cybershare.utep.edu/ELSEWeb/elseweb-lifemapper.owl#">http://ontology.cybershare.utep.edu/ELSEWeb/elseweb-lifemapper.owl#</a><br>
-                        <a href="http://ontology.cybershare.utep.edu/ELSEWeb/linked-data/lifemapper/occurrences-v2/species-occurrences-v2.owl#">http://ontology.cybershare.utep.edu/ELSEWeb/linked-data/lifemapper/occurrences-v2/species-occurrences-v2.owl#</a><br>
-                        <a href="http://ontology.cybershare.utep.edu/ELSEWeb/elseweb-lifemapper-parameters.owl#">http://ontology.cybershare.utep.edu/ELSEWeb/elseweb-lifemapper-parameters.owl#</a><br>
-                        <a href="http://ontology.cybershare.utep.edu/ELSEWeb/mappings/elseweb-mappings.owl#">http://ontology.cybershare.utep.edu/ELSEWeb/mappings/elseweb-mappings.owl#</a>
+                         <a href="<?php echo ONTPREFIX."elseweb-lifemapper.owl";?>"><img class="img-responsive" width="470" src="<?php echo base_url(IMAGES."elseweb-lifemapper.png");?>" alt="elseweb-lifemapper"></a>
                     </div>
                 </div>
             </div>
@@ -53,7 +53,7 @@
                     <h3 class="title">ELSEWeb EDAC to Lifemapper</h3>
                     <p>The <a href="<?php echo ONTPREFIX."elseweb-lifemapper.owl";?>">edac-to-lifemapper ontology</a> describes the relationships between user experiment specifications and Lifemapper experiment specifications. Users experiment specifications reference resources such as EDAC WCS datasets, which Lifemapper is unable to directly process.  Lifemapper experiment specifications expect input data as “Posted TIFFs” and to be associated with posted “Scenarios”. Therefore, rounds of transformations must ensue before feeding EDAC data to Lifemapper. The edac-to-lifemapper ontology specifies the intermediate forms and how they relate from the source EDAC data and the required Lifemapper scenarios. From these intermediate form descriptions, tools such as <a href”http://sadiframework.org/content/tag/cardioshare/”>SADI SHARE client</a> can identify services that support the transformations of EDAC data to forms ingestible by Lifemapper.
 </p>
-                    <a href="<?php echo ONTPREFIX."elseweb-lifemapper.owl";?>"><img width="900" class="img-responsive displayed" src="<?php echo base_url(IMAGES."edac-to-lifemapper.png");?>" alt="edac-to-lifemapper"></a>
+                    <a href="<?php echo ONTPREFIX."elseweb-lifemapper.owl";?>"><img width="900" class="img-responsive" src="<?php echo base_url(IMAGES."edac-to-lifemapper.png");?>" alt="edac-to-lifemapper"></a>
                 </div>
             </div>
         </div>
