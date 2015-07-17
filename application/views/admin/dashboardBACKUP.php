e27cd072d7ad2c78bf2ee8b37d06df11a96c2596
Luis Garnica <lagc03@gmail.com>
2015-07-17 15:17:09 -0600

    Back to old figures.

--- application/views/admin/dashboardBACKUP.php
+++ application/views/admin/dashboardBACKUP.php
@@ -31,7 +31,7 @@ class Dashboard extends CI_Controller
     }
 
    
-} 
+} ?>
 
 <!DOCTYPE html>
 <html lang="en">
--- application/views/pages/ontologies.php
+++ application/views/pages/ontologies.php
@@ -19,11 +19,25 @@
                             elseweb-data ontology to describe data sets published by EDAC. Dashed boxes outline concepts of
                             the ontology that extend existing ontologies and vocabularies.
                         </p>
-                        <a href="<?php echo ONTPREFIX."elseweb-edac.owl";?>"><img class="img-responsive displayed" width="850" src="<?php echo base_url(IMAGES."elseweb-data-v2.png");?>" alt="elseweb-data"></a>
-                        <a href="http://ontology.cybershare.utep.edu/ELSEWeb/elseweb-data.owl#">http://ontology.cybershare.utep.edu/ELSEWeb/elseweb-data.owl#</a> <br>
-                        <a href="http://ontology.cybershare.utep.edu/ELSEWeb/elseweb-edac.owl#">http://ontology.cybershare.utep.edu/ELSEWeb/elseweb-edac.owl#</a><br>
-                        <a href="http://ontology.cybershare.utep.edu/ELSEWeb/linked-data/edac/services/wcs-services.owl#">http://ontology.cybershare.utep.edu/ELSEWeb/linked-data/edac/services/wcs-services.owl#</a><br>
-                        <a href="http://ontology.cybershare.utep.edu/ELSEWeb/mappings/elseweb-mappings.owl#">http://ontology.cybershare.utep.edu/ELSEWeb/mappings/elseweb-mappings.owl#</a>
+                        <div class="row">
+                            <div class="col-lg-6 col-sm-6">
+                                <h4 class="title">ELSEWEB-Data</h4> 
+                                <a href="<?php echo ONTPREFIX."elseweb-data.owl";?>"><img class="img-responsive displayed" width="850" src="<?php echo base_url(IMAGES."elseweb-data.png");?>" alt="elseweb-data"></a> 
+                            </div>
+                            <div class="col-lg-6 col-sm-6">
+                                <h4 class="title">ELSEWEB-EDAC</h4> 
+                                <a href="<?php echo ONTPREFIX."elseweb-edac.owl";?>"><img class="img-responsive displayed" width="850" src="<?php echo base_url(IMAGES."elseweb-edac.png");?>" alt="elseweb-edac"></a>  
+                            </div>
+                        </div>
+                        
+                        <div class="row">
+                          <div class="col-lg-12 col-sm-12" style="margin-top: 10px">  
+                              <a href="http://ontology.cybershare.utep.edu/ELSEWeb/elseweb-data.owl#">http://ontology.cybershare.utep.edu/ELSEWeb/elseweb-data.owl#</a> <br>
+                              <a href="http://ontology.cybershare.utep.edu/ELSEWeb/elseweb-edac.owl#">http://ontology.cybershare.utep.edu/ELSEWeb/elseweb-edac.owl#</a><br>
+                              <a href="http://ontology.cybershare.utep.edu/ELSEWeb/linked-data/edac/services/wcs-services.owl#">http://ontology.cybershare.utep.edu/ELSEWeb/linked-data/edac/services/wcs-services.owl#</a><br>
+                              <a href="http://ontology.cybershare.utep.edu/ELSEWeb/mappings/elseweb-mappings.owl#">http://ontology.cybershare.utep.edu/ELSEWeb/mappings/elseweb-mappings.owl#</a>
+                          </div>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -33,15 +47,27 @@
             <div class="hiring">
                 <div class="col-lg-12 col-sm-12">
                     <div class="content">
-                        <h3 class="title">ELSEWeb Modeling and ELSEWeb Lifemapper</h3>
-                        <p>The elseweb-model ontology (blue nodes) provides concepts for describing biodiversity modeling algorithms, their inputs, outputs, parameters, and services providing these algorithms. The elseweb-lifemapper ontology (yellow nodes), extends and instantiates the elseweb-model ontology to describe SDM services provided by Lifemapper. Dashed boxes outline concepts of the ontology that extend existing ontologies and vocabularies.</p>
+                        <h3 class="title">ELSEWeb Modelling and ELSEWeb Lifemapper</h3>
+                        <p>The elseweb-modelling ontology (blue nodes) provides concepts for describing biodiversity modeling algorithms, their inputs, outputs, parameters, and services providing these algorithms. The elseweb-lifemapper ontology (yellow nodes), extends and instantiates the elseweb-modelling ontology to describe SDM services provided by Lifemapper. Dashed boxes outline concepts of the ontology that extend existing ontologies and vocabularies.</p>
                         <p>The <a href="<?php echo ONTPREFIX."elseweb-lifemapper.owl";?>">elseweb-lifemapper</a> describes specific <a href="<?php echo LODSPEAKR."instances/elseweb_lifemapper:LifemapperAlgorithm";?>">modeling algorithms</a> supported by <a href="http://lifemapper.org">Lifemapper</a> as well as corresponding parameter sets. The parameter descriptions contain minimum and maximum “hasValue” property restrictions to define the legal ranges of values bound to specific parameters. This ontology also contains a collection of <a href="<?php echo LODSPEAKR."instances/elseweb_lifemapper:SpeciesOccurrenceDataset";?>">species occurrence data</a> that can be referenced in experiments.</p>
-                        <a href="<?php echo ONTPREFIX."elseweb-lifemapper.owl";?>"><img class="img-responsive displayed" width="850" src="<?php echo base_url(IMAGES."elseweb-modeling-v2.jpeg");?>" alt="elseweb-modeling" style="margin-bottom: 20px;"></a>
-                        <a href="http://ontology.cybershare.utep.edu/ELSEWeb/elseweb-modelling.owl#">http://ontology.cybershare.utep.edu/ELSEWeb/elseweb-modelling.owl#</a><br>
-                        <a href="http://ontology.cybershare.utep.edu/ELSEWeb/elseweb-lifemapper.owl#">http://ontology.cybershare.utep.edu/ELSEWeb/elseweb-lifemapper.owl#</a><br>
-                        <a href="http://ontology.cybershare.utep.edu/ELSEWeb/linked-data/lifemapper/occurrences-v2/species-occurrences-v2.owl#">http://ontology.cybershare.utep.edu/ELSEWeb/linked-data/lifemapper/occurrences-v2/species-occurrences-v2.owl#</a><br>
-                        <a href="http://ontology.cybershare.utep.edu/ELSEWeb/elseweb-lifemapper-parameters.owl#">http://ontology.cybershare.utep.edu/ELSEWeb/elseweb-lifemapper-parameters.owl#</a><br>
-                        <a href="http://ontology.cybershare.utep.edu/ELSEWeb/mappings/elseweb-mappings.owl#">http://ontology.cybershare.utep.edu/ELSEWeb/mappings/elseweb-mappings.owl#</a>
+                        <div class="row">
+                            <div class="col-lg-6 col-sm-6">
+                                <h4 class="title">ELSEWEB-Modelling</h4> 
+                                <a href="<?php echo ONTPREFIX."elseweb-modelling.owl";?>"><img class="img-responsive displayed" width="850" src="<?php echo base_url(IMAGES."elseweb-modelling.png");?>" alt="elseweb-modelling"></a> 
+                            </div>
+                            <div class="col-lg-6 col-sm-6">
+                                <h4 class="title">ELSEWEB-Lifemapper</h4> 
+                                <a href="<?php echo ONTPREFIX."elseweb-lifemapper.owl";?>"><img class="img-responsive displayed" width="850" src="<?php echo base_url(IMAGES."elseweb-lifemapper.png");?>" alt="elseweb-lifemapper"></a>  
+                            </div>
+                        </div>
+                        
+                        <div class="row">
+                             <a href="http://ontology.cybershare.utep.edu/ELSEWeb/elseweb-modelling.owl#">http://ontology.cybershare.utep.edu/ELSEWeb/elseweb-modelling.owl#</a><br>
+                             <a href="http://ontology.cybershare.utep.edu/ELSEWeb/elseweb-lifemapper.owl#">http://ontology.cybershare.utep.edu/ELSEWeb/elseweb-lifemapper.owl#</a><br>
+                             <a href="http://ontology.cybershare.utep.edu/ELSEWeb/linked-data/lifemapper/occurrences-v2/species-occurrences-v2.owl#">http://ontology.cybershare.utep.edu/ELSEWeb/linked-data/lifemapper/occurrences-v2/species-occurrences-v2.owl#</a><br>
+                             <a href="http://ontology.cybershare.utep.edu/ELSEWeb/elseweb-lifemapper-parameters.owl#">http://ontology.cybershare.utep.edu/ELSEWeb/elseweb-lifemapper-parameters.owl#</a><br>
+                             <a href="http://ontology.cybershare.utep.edu/ELSEWeb/mappings/elseweb-mappings.owl#">http://ontology.cybershare.utep.edu/ELSEWeb/mappings/elseweb-mappings.owl#</a>
+                        </div>     
                     </div>
                 </div>
             </div>
