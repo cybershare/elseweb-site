37bf41864e1ee73fd1faf149dad570410c4a2254
Luis Garnica <lagarnicachavira@miners.utep.edu>
2015-09-24 14:43:30 -0600

    Updated resources section.

--- application/views/pages/publications.php
+++ application/views/pages/publications.php
@@ -9,23 +9,33 @@
             <!--feature end-->
         </div>
 
-        <div class="row gray-box">
-             <div class="hiring">
-                 <div class="col-md-6 col-md-offset-3">
+        <div class="row">
+                 <div class="col-md-12">
                      <div class="content">
-                         <p>
-                             <a href="https://www.aaai.org/ocs/index.php/FSS/FSS13/paper/viewFile/7631/7488" target="_blank" >    
-                                 Del Rio, N., Villanueva-Rosales, N., Pennington, D., Benedict, K., Stewart, A., & Grady, C. J. (2013, December). Elseweb meets sadi: Supporting data-to-model integration for biodiversity forecasting. In Discovery Informatics Symposium.
-                             </a>
-                         </p>
-                         <p>
-                             <a href="" target="_blank" >    
-                                 Villanueva-Rosales, N., Garnica Chavira, L., Del Rio, N., Pennington, D.: eScience through the integration of data and models: a biodiversity scenario. In: 11th IEEE International Conference on eScience (2015).
-                             </a>
-                         </p>
+                         <table class="table table-striped table-bordered">
+                             <tr>
+                                <th>Publication</th>
+                                <th>Download</th>
+                             </tr>
+                             <tr>
+                                 <td>Del Rio, N., Villanueva-Rosales, N., Pennington, D., Benedict, K., Stewart, A., & Grady, C. J. (2013, December). Elseweb meets sadi: Supporting data-to-model integration for biodiversity forecasting. In Discovery Informatics Symposium.</td>
+                                 <td class="text-center">
+                                     <button type="button" class="btn btn-link">
+                                         <a  href="https://www.aaai.org/ocs/index.php/FSS/FSS13/paper/viewFile/7631/7488" target="_blank"><span class="glyphicon glyphicon-paperclip"></span></a>
+                                     </button>
+                                 </td>
+                             </tr>
+                             <tr>
+                                 <td>Villanueva-Rosales, N., Garnica Chavira, L., Del Rio, N., Pennington, D.: eScience through the integration of data and models: a biodiversity scenario. In: 11th IEEE International Conference on eScience (2015) --Pending Web Publication--</td>
+                                 <td class="text-center">
+                                    <button type="button" class="btn btn-link">
+                                        <a  href="" target="_blank"><span class="glyphicon glyphicon-paperclip"></span></a>
+                                    </button>
+                                 </td>
+                             </tr>
+                         </table>
                      </div>
                  </div>
-             </div>
          </div>
         
         <div class="row">
@@ -36,17 +46,33 @@
             <!--feature end-->
         </div>
        
-          <div class="row gray-box">
-             <div class="hiring">
-                 <div class="col-md-6 col-md-offset-3">
+        <div class="row">
+                 <div class="col-md-12">
                      <div class="content">
-                         <p>
-                             <a href="<?php echo base_url(DOCS."UsabilityStudyReportFinal.pdf");?>" target="_blank" >ELSEWeb GUI - Usability Study Report</a> <br>
-                             <a href="<?php echo base_url(DOCS."IRBNetDocument.pdf");?>" target="_blank">Institutional Review Board - Exemption Letter</a>
-                         </p>
+                         <table class="table table-striped table-bordered">
+                             <tr>
+                                <th>Document</th>
+                                <th class="text-center"> Download</th>
+                             </tr>
+                             <tr>
+                                 <td>ELSEWeb GUI - Preliminary Usability Study Report</td>
+                                 <td class="text-center">
+                                     <button type="button" class="btn btn-link">
+                                         <a  href="<?php echo base_url(DOCS."UsabilityStudyReportFinal.pdf");?>" target="_blank"><span class="glyphicon glyphicon-paperclip"></span></a>
+                                     </button>
+                                 </td>
+                             </tr>
+                             <tr>
+                                 <td>Institutional Review Board - Exemption Letter</td>
+                                 <td class="text-center">
+                                    <button type="button" class="btn btn-link">
+                                        <a  href="<?php echo base_url(DOCS."IRBNetDocument.pdf");?>" target="_blank"><span class="glyphicon glyphicon-paperclip"></span></a>
+                                    </button>
+                                 </td>
+                             </tr>
+                         </table>
                      </div>
                  </div>
-             </div>
          </div>
         
         <div class="row">
@@ -56,18 +82,64 @@
             </div>
             <!--feature end-->
         </div>
-       
-          <div class="row gray-box">
-             <div class="hiring">
-                 <div class="col-md-6 col-md-offset-3">
+        
+        
+        <div class="row">
+                 <div class="col-md-12">
                      <div class="content">
-                         <p>
-                             <a href="<?php echo base_url();?>" target="_blank" >ELSEWeb Frontend Repository</a> <br>
-                             <a href="<?php echo base_url();?>" target="_blank">ELSEWeb Backend Repository</a>
-                         </p>
+                         <table class="table table-striped table-bordered">
+                             <tr>
+                                <th>Repository</th>
+                                <th class="text-center">Link</th>
+                             </tr>
+                             <tr>
+                                 <td>ELSEWeb Frontend Repository</td>
+                                 <td class="text-center">
+                                     <button type="button" class="btn btn-link">
+                                         <a  href="https://github.com/cybershare/elseweb-site" target="_blank"><span class="glyphicon glyphicon-link"></span></a>
+                                     </button>
+                                 </td>
+                             </tr>
+                             <tr>
+                                 <td>ELSEWeb Backend Repository</td>
+                                 <td class="text-center">
+                                    <button type="button" class="btn btn-link">
+                                        <a  href="https://github.com/cybershare/elseweb-v2" target="_blank"><span class="glyphicon glyphicon-link"></span></a>
+                                    </button>
+                                 </td>
+                             </tr>
+                         </table>
                      </div>
                  </div>
-             </div>
+         </div>
+    
+         <div class="row">
+            <!--feature start-->
+            <div class="text-center feature-head">
+                <h2>ELSEWeb Ontologies</h2>
+            </div>
+            <!--feature end-->
+        </div>
+        
+        <div class="row">
+                 <div class="col-md-12">
+                     <div class="content">
+                         <table class="table table-striped table-bordered">
+                             <tr>
+                                <th>Ontology Namespace</th>
+                                <th class="text-center">Link</th>
+                             </tr>
+                             <tr>
+                                 <td>http://ontology.cybershare.utep.edu/ELSEWeb/</td>
+                                 <td class="text-center">
+                                     <button type="button" class="btn btn-link">
+                                         <a  href="http://ontology.cybershare.utep.edu/ELSEWeb/" target="_blank"><span class="glyphicon glyphicon-link"></span></a>
+                                     </button>
+                                 </td>
+                             </tr>
+                         </table>
+                     </div>
+                 </div>
          </div>
         
     </div>    
